0fcd3df63899965968761673c042b87791398298
atsukoba <atsuya_wgp4346@keio.jp>
2018-11-13 13:29:49 +0900

    Add type annotation and numpy style docstrings to each function in this module file.

--- w2v.py
+++ w2v.py
@@ -5,25 +5,41 @@ import os
 import numpy as np
 
 
-def train_word2vec(sentence_matrix, vocabulary_inv,
-                   num_features=300, min_word_count=1, context=10):
+def train_word2vec(sentence_matrix: , vocabulary_inv,
+                   num_features=300, min_word_count=1, context=10) -> dict:
     """
     Trains, saves, loads Word2Vec model
     Returns initial weights for embedding layer.
-   
-    inputs:
-    sentence_matrix # int matrix: num_sentences x max_sentence_len
-    vocabulary_inv  # dict {int: str}
-    num_features    # Word vector dimensionality                      
-    min_word_count  # Minimum word count                        
-    context         # Context window size 
+
+    Parameters
+    ----------
+    sentence_matrix : int
+        matrix: num_sentences x max_sentence_len
+
+    vocabulary_inv : dict {int: str}
+
+    num_features : int
+        Word vector dimensionality
+
+    min_word_count : int
+        Minimum word count
+
+    context : int
+        Context window size
+
+    Returns
+    -------
+    embedding_weights : dict
+
     """
     model_dir = 'models'
     model_name = "{:d}features_{:d}minwords_{:d}context".format(num_features, min_word_count, context)
     model_name = join(model_dir, model_name)
+
     if exists(model_name):
         embedding_model = word2vec.Word2Vec.load(model_name)
         print('Load existing Word2Vec model \'%s\'' % split(model_name)[-1])
+
     else:
         # Set values for various parameters
         num_workers = 2  # Number of threads to run in parallel
@@ -32,11 +48,12 @@ def train_word2vec(sentence_matrix, vocabulary_inv,
         # Initialize and train the model
         print('Training Word2Vec model...')
         sentences = [[vocabulary_inv[w] for w in s] for s in sentence_matrix]
+        # training genim skipgram model for word embedding
         embedding_model = word2vec.Word2Vec(sentences, workers=num_workers,
                                             size=num_features, min_count=min_word_count,
                                             window=context, sample=downsampling)
 
-        # If we don't plan to train the model any further, calling 
+        # If we don't plan to train the model any further, calling
         # init_sims will make the model much more memory-efficient.
         embedding_model.init_sims(replace=True)
 
@@ -50,6 +67,7 @@ def train_word2vec(sentence_matrix, vocabulary_inv,
     embedding_weights = {key: embedding_model[word] if word in embedding_model else
                               np.random.uniform(-0.25, 0.25, embedding_model.vector_size)
                          for key, word in vocabulary_inv.items()}
+
     return embedding_weights
 
 
